5e95cd837c00f642caec431ee18b83642bd1173f
cr <rorycheng@yeah.net>
2019-05-21 22:50:26 +0800

    update

--- src/main/java/com/SpringBootDemo/controller/FindCompany.java
+++ src/main/java/com/SpringBootDemo/controller/FindCompany.java
@@ -3,7 +3,10 @@ package com.SpringBootDemo.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +35,11 @@ public class FindCompany {
 		String user="szc";
 		List<Map<String, Object>> list=companyMapper.findCompanyInfo(user);
 		return list;
+	}
+	
+	@RequestMapping("/say/{info}")
+	public String say(@PathVariable String info) {
+		return "say"+info;
+		
 	}
 }
--- src/test/java/com/SpringBootDemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/SpringBootDemo/SpringBootDemoApplicationTests.java
@@ -24,8 +24,6 @@ public class SpringBootDemoApplicationTests {
 		for (CompanyInfo companyInfo : list) {
 			System.out.println(companyInfo.getScode());
 		}
-		
-		
 	}
 
 }
