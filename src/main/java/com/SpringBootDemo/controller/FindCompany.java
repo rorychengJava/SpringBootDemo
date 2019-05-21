64465cac457158288d7162d931490a27fb90d9ab
cr <cr@cnbisoft.com>
2019-05-21 11:21:12 +0800

    新增mapper接受List<Map>

--- src/main/java/com/SpringBootDemo/controller/FindCompany.java
+++ src/main/java/com/SpringBootDemo/controller/FindCompany.java
@@ -1,33 +1,36 @@
 package com.SpringBootDemo.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.SpringBootDemo.mapper.CompanyMapper;
 import com.SpringBootDemo.util.CompanyInfo;
 
-
-@Controller
+@RestController
 public class FindCompany {
 
 	@Autowired
 	private CompanyMapper companyMapper;
 	
 	@RequestMapping("/hello")
-	@ResponseBody
 	public String hello() {
 		return "hello";
 	}
 	
 	@RequestMapping("/findCompany")
-	@ResponseBody
 	public List<CompanyInfo> company () {
 		List<CompanyInfo> list=companyMapper.findCompanyById("1001");
-		
+		return list;
+	}
+	
+	@RequestMapping("/findCompanyInfo")
+	public List<Map<String, Object>> findCompanyInfo(){
+		String user="szc";
+		List<Map<String, Object>> list=companyMapper.findCompanyInfo(user);
 		return list;
 	}
 }
--- src/main/java/com/SpringBootDemo/mapper/CompanyMapper.java
+++ src/main/java/com/SpringBootDemo/mapper/CompanyMapper.java
@@ -4,6 +4,7 @@
 package com.SpringBootDemo.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -20,4 +21,8 @@ public interface CompanyMapper {
 	
 	//根据公司ID查找
 	 List<CompanyInfo> findCompanyById(String scode);
+	 
+	 //根据用户名查找公司对应信息
+	 //由于该查询无相对应实体类字段可以接受，故直接使用map的key-value对应关系进行存数据放到list中
+	 List<Map<String, Object>> findCompanyInfo(String user);
 }
