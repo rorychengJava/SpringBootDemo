1c5b409b793f26e35853a66e258f5532d056d486
cr <cr@cnbisoft.com>
2019-05-30 08:45:01 +0800

    更新部分内容

--- src/main/java/com/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/SpringBootDemo/SpringBootDemoApplication.java
@@ -3,15 +3,29 @@ package com.SpringBootDemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.SpringBootDemo")
 @MapperScan("com.SpringBootDemo.mapper.*")
-public class SpringBootDemoApplication {
+
+/*public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
+}*/
+
+	public class SpringBootDemoApplication extends SpringBootServletInitializer{
+		
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+			return builder.sources(SpringBootDemoApplication.class);
+	}
+		public static void main(String[] args) {
+			SpringApplication.run(SpringBootDemoApplication.class, args);
+		}
+	
 }
