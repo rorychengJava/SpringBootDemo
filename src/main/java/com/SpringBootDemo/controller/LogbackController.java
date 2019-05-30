7a7aeba92d8056b49600b8a58d5cd56c8994763b
cr <cr@cnbisoft.com>
2019-05-30 22:19:40 +0800

    提交logback.xml文件

---
+++ src/main/java/com/SpringBootDemo/controller/LogbackController.java
@@ -0,0 +1,22 @@
+package com.SpringBootDemo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogbackController {
+	private Logger logger=LoggerFactory.getLogger(LogbackController.class);
+	
+	@RequestMapping("/log")
+	public String log() {
+		logger.debug("debug日志");
+		logger.info("info日志");
+		logger.warn("warn日志");
+		logger.error("error日志");
+		return "log";
+	}
+	
+
+}
