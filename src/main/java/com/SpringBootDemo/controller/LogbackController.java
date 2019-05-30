9e34da6802f505f174a7707ef062c9de2346c7b8
cr <rorycheng@yeah.net>
2019-05-30 23:02:39 +0800

    热部署和web自动化配置

--- src/main/java/com/SpringBootDemo/controller/LogbackController.java
+++ src/main/java/com/SpringBootDemo/controller/LogbackController.java
@@ -2,8 +2,11 @@ package com.SpringBootDemo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringBootDemo.SpringBootDemoApplication;
 
 @RestController
 public class LogbackController {
@@ -18,5 +21,7 @@ public class LogbackController {
 		return "log";
 	}
 	
-
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackController.class, args);
+	}
 }
